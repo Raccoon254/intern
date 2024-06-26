98f73d5774d4765301f149fcc80f92d7584069b2
raccoon254 <tomsteve187@gmail.com>
2024-06-26 11:33:30 +0300

    Auth route cleanup

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -8,12 +8,14 @@ const prisma = new PrismaClient()
 
 const authOptions = {
     providers: [
+
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET,
 
             callbackUrl: process.env.CALLBACK,
         }),
+
         CredentialsProvider({
             name: 'Credentials',
             credentials: {
@@ -25,13 +27,14 @@ const authOptions = {
                 const password = credentials.password
                 const email = credentials.email
 
-                const user = await User.findOne({ where: { Email: email } })
+                const user = await prisma.user.findUnique({ where: { Email: email } })
                 if (!user) return null
                 const match = await compare(password, user.Password)
                 if (match) {
                     return {
-                        id: user.UserID,
-                        email: user.Email,
+                        id: user.id,
+                        email: user.email,
+                        role: user.role
                     }
                 } else {
                     return null
@@ -44,7 +47,7 @@ const authOptions = {
         error: '/auth/login',
         newUser: '/auth/register'
     },
-    secret: process.env.NEXTAUTH_SECRET,
+    secret: process.env.AUTH_SECRET,
     session: {
         jwt: true,
         maxAge: 30 * 24 * 60 * 60
