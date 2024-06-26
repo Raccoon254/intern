fa6e8523e4997cdc518929b6ff91cf506b39b306
raccoon254 <tomsteve187@gmail.com>
2024-06-26 20:07:18 +0300

    Complete create and read all departments

---
+++ app/api/departments/route.js
@@ -0,0 +1,28 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export async function GET(req, res) {
+    try {
+        const users = await prisma.department.findMany();
+        return new Response(JSON.stringify(users), { status: 200 });
+    } catch (error) {
+        return new Response(JSON.stringify({ error: 'Failed to fetch users' }), { status: 500 });
+    }
+}
+
+export async function POST(req, res){
+    try{
+        const { name, organizationId } = await req.json();
+        const department = await prisma.department.create({
+            data: {
+                name,
+                organizationId
+            }
+        });
+        return new Response(JSON.stringify(department), { status: 200 });
+    }catch (error){
+        console.error(error);
+        return new Response(JSON.stringify({ error: 'Failed to create department' }), { status: 500 });
+    }
+}
