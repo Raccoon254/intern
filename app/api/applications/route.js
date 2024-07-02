eaef67d81a52af4c38f1dfaf416e8ef123776d7f
raccoon254 <tomsteve187@gmail.com>
2024-07-02 08:39:54 +0300

    Created the applications post endpoint

--- app/api/applications/route.js
+++ app/api/applications/route.js
@@ -17,3 +17,29 @@ export async function GET(req, res){
         return new Response(JSON.stringify({ error: 'Failed to fetch applications' }), { status: 500 })
     }
 }
+
+export async function POST(req, res){
+    try{
+
+        const { coverLetter, resume, studentId, jobPostingId } = await req.json()
+        
+        if ( !coverLetter || !resume || !studentId || !jobPostingId ) {
+            return new Response(JSON.stringify({ error: 'All fields are required' }), { status: 400 })
+        }
+
+        const application = await prisma.application.create({
+            data: {
+                coverLetter,
+                resume,
+                status: 'PENDING',
+                studentId,
+                jobPostingId
+            }
+        })
+
+        return new Response(JSON.stringify(application), { status: 200 })
+    }catch(error){
+        console.log(error)
+        return new Response(JSON.stringify({ error: 'Failed to create application \n' + error }), { status: 500 })
+    }
+}
